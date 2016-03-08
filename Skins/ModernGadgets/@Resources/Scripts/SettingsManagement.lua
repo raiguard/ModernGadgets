027d7ccf34d6c55e7ad7a06c5ac0d08e038cb7a2
iamanai <iamanai@users.noreply.github.com>
2016-03-08 09:40:21 -0700

    Began work on new settings system (mobile edit)

---
+++ Skins/ModernGadgets/@Resources/Scripts/SettingsManagement.lua
@@ -0,0 +1,42 @@
+; MODERN GADGETS SETTINGS MANAGEMENT
+; Version 1.0.0
+; By iamanai
+;
+; ------------------------------
+; CHANGELOG
+; v1.0.0 - 2016-??-??: Initial release
+; ------------------------------
+;
+
+function Initialize()
+
+  fileName = 'ModernGadgetsSettings.inc
+  filePath = SKIN:GetVariable('SETTINGSPATH') .. FileName
+
+end
+
+function Update()
+
+
+end
+
+; Checks Rainmeter's settings folder for a file named 'ModernGadgetsSettings.inc'
+function CheckForSettingsFile()
+
+
+end
+
+function ParseFile(table)
+
+
+end
+
+function CrossReference(table1, table2)
+
+
+end
+
+function RebuildFile(table)
+
+
+end
