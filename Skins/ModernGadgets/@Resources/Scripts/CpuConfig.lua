7ba6848c5398b9bdda383a3c3c0882ffdf3ca10c
iamanai <iamanaiofficial@gmail.com>
2016-05-07 00:57:41 -0600

    Lots of work on CPU gadget rewrites, worked on Cpu Config script, added FiraMono font (unused), added "redalert" sound

--- Skins/ModernGadgets/@Resources/Scripts/CpuConfig.lua
+++ Skins/ModernGadgets/@Resources/Scripts/CpuConfig.lua
@@ -1,6 +1,6 @@
-; MODERNGADGETS CPU CONFIG SCRIPT
-; Based on work by SilverAzide
-; Written by iamanai
+-- MODERNGADGETS CPU CONFIG SCRIPT
+-- Based on work by SilverAzide
+-- Written by iamanai
 
 function Initialize() end
 
@@ -8,10 +8,13 @@ function Update() end
 
 function ConfigCores(cores, threads)
 
-  if threads % 2 = 0 then
+  if threads % 2 == 0 then
     for i=1,16 do
-      if not (i / 2) > cores then SKIN:Bang('!WriteKeyValue', 'Variables', 'hideCore' .. i, '0')
-      else SKIN:Bang('!WriteKeyValue', 'Variables', 'hideCore' .. i, '1') end
+      if not (i / 2) > cores then
+        SKIN:Bang('!SetOptionGroup', 'CpuCore' .. i, 'Hidden', '0')
+        SKIN:Bang('!WriteKeyValue', 'Core' .. i .. 'LabelString', 'Hidden', '0')
+        SKIN:Bang('!WriteKeyValue', 'Core' .. i .. 'LabelString', 'Y', 'Core' .. (i - 1) .. 'LabelString:Y')
+      else SKIN:Bang('!SetOptionGroup', 'CpuCore' .. i, 'Hidden', '1') end
     end
   else
     for i=1,16 do
@@ -19,5 +22,6 @@ function ConfigCores(cores, threads)
       else SKIN:Bang('!WriteKeyValue', 'Variables', 'hideCore' .. i, '1') end
     end
   end
+
 
 end
