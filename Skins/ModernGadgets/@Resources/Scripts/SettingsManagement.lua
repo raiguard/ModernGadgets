99692981c5e0b1734338ee14d42512e281fd4c5d
iamanai <iamanaiofficial@gmail.com>
2016-07-04 13:33:16 -0600

    Finished settings system refactor (completely!)

--- Skins/ModernGadgets/@Resources/Scripts/SettingsManagement.lua
+++ Skins/ModernGadgets/@Resources/Scripts/SettingsManagement.lua
@@ -3,8 +3,20 @@
 -- By iamanai
 --
 -- This script is used by the setup skin to manage the external settings files.
--- ReadIni and WriteIni scripts created by jsmorley, taken with permission from
--- the Rainmeter snippits page.
+-- ReadIni and WriteIni scripts created by jsmorley, edited by iamanai. Used
+-- with permission from the Rainmeter snippits page
+-- OrderedTableSimple script taken from
+-- http://lua-users.org/wiki/OrderedTableSimple, used with permission.
+--
+-- --------------------
+-- CHANGELOG
+-- v1.0.0 - 2016-6-4
+--  - Initial release
+-- --------------------
+--
+-- TO-DO:
+--  - Add capabilities to ReadIni and WriteIni to handle intentional line spaces and comments
+--  - Add check for extraneous values in settings files, so they can be deleted
 --
 
 isDbg = true
@@ -19,14 +31,6 @@ function Initialize()
                 'GpuSettings.inc',
                 'DisksSettings.inc' }
 
-  tempTable = {
-    { Variables = {
-      { tempUOM = 'C' },
-      { fileRevision = '1.0.0' }
-      }
-    }
-  }
-
   filesPath = SKIN:GetVariable('SETTINGSPATH') .. 'ModernGadgetsSettings\\'
 
   rFilesPath = SKIN:GetVariable('@') .. 'ReferenceFiles\\'
@@ -40,18 +44,11 @@ function Update() end
 -- runs once on initial skin load (not a refresh)
 function SettingsProtocol()
 
-  -- print_r(ctrlTable)
-  -- LogHelper('mgVersion: ' .. tostring(ctrlTable[Variables][mgVersion]) .. ' | fileRevision: ' .. tostring(ctrlTable[Variables][fileRevision]), 'Debug')
-  -- WriteIni(ctrlTable, rFilesPath .. 'Temp.inc')
-
   -- check if external control file exists
   local ctrlFile = io.open(filesPath .. fileNames[1])
   if ctrlFile == nil then
     LogHelper('Creating external settings files', 'Notice')
-
     CreateFiles()
-
-
   else
     io.close(ctrlFile)
     LogHelper('Settings files found', 'Debug')
@@ -65,20 +62,17 @@ function SettingsProtocol()
       LogHelper('Settings files up-to-date', 'Notice')
     else
       LogHelper('Updating settings files', 'Notice')
-
       UpdateFiles()
     end
-
   end
-
-  print_r(tempTable)
-
 end
 
 function CreateFiles()
 
+  -- create file directory
   os.execute("mkdir " .. filesPath)
 
+  -- write entire contents of reference files to the corresponding settings files
   for i=1,7 do
     WriteIni(ReadIni(rFilesPath .. fileNames[i]), filesPath .. fileNames[i])
   end
@@ -87,28 +81,39 @@ end
 
 function UpdateFiles()
 
-  -- create tables for every file
-  local tableOfRefTables = {}
-  local tableOfTables = {}
+  -- for all settings files
+  for y=1,7 do
+    -- create file tables for reference file and actual file, constructor table
+    local fileTable = ReadIni(filesPath .. fileNames[y])
+    local refFileTable = ReadIni(rFilesPath .. fileNames[y])
+    local newFileTable = newT()
 
-  for i=1,7 do
-    tableOfRefTables[i] = ReadIni(rFilesPath .. fileNames[i])
-    tableOfTables[i] = ReadIni(filesPath .. fileNames[i])
+    -- for every section in the file
+    for i,v in refFileTable:opairs() do
+      newFileTable[i] = newT()
+      -- for every key=value pair in the section
+      for a,b in refFileTable[i]:opairs() do
+        -- if value of current key is same for both files, or if actual file does not have key
+        if refFileTable[i][a] == fileTable[i][a] or fileTable[i][a] == nil then
+          newFileTable[i][a] = b
+        else
+          LogHelper('Conflict: ' .. a, 'Debug')
+          newFileTable[i][a] = fileTable[i][a]
+        end
+      end
+    end
 
-    table.sort(tableOfRefTables)
-    table.sort(tableOfTables)
-
-    print_r(tableOfRefTables)
+    -- write constructor table to settings file
+    WriteIni(newFileTable, filesPath .. fileNames[y])
+    LogHelper('Finished cross-reference for ' .. fileNames[y], 'Debug')
   end
-
-  -- print_r(tableOfRefTables)
 
 end
 
--- Created by jsmorley, parses a .ini or .inc file into a table of Table[Section][Key] = Value
+-- parses a INI formatted text file into a 'TableName[Section][Key] = Value' table using the newT() method
 function ReadIni(inputfile)
 	local file = assert(io.open(inputfile, 'r'), 'Unable to open ' .. inputfile)
-	local tbl, section = {}
+	local tbl, section = newT()
 	local num = 0
 	for line in file:lines() do
 		num = num + 1
@@ -116,9 +121,11 @@ function ReadIni(inputfile)
 			local key, command = line:match('^([^=]+)=(.+)')
 			if line:match('^%s-%[.+') then
 				section = line:match('^%s-%[([^%]]+)')
-				if not tbl[section] then tbl[section] = {} end
+        -- LogHelper(section, 'Debug')
+				if not tbl[section] then tbl[section] = newT() end
 			elseif key and command and section then
 				tbl[section][key:match('^s*(%S*)%s*$')] = command:match('^s*(.-)%s*$')
+        -- LogHelper(key .. '=' .. command, 'Debug')
 			elseif #line > 0 and section and not key or command then
 				print(num .. ': Invalid property or value.')
 			end
@@ -129,23 +136,71 @@ function ReadIni(inputfile)
 	return tbl
 end
 
--- Also created by jsmorley, takes a Table[Section][Key] = Value list and writes the values into a .ini or .inc file
+-- takes a table created by ReadIni and writes the contents into a INI formatted text file
 function WriteIni(inputtable, filename)
 	assert(type(inputtable) == 'table', ('WriteIni must receive a table. Received %s instead.'):format(type(inputtable)))
 
 	local file = assert(io.open(filename, 'w'), 'Unable to open ' .. filename)
 	local lines = {}
 
-	for section, contents in pairs(inputtable) do
+	for section, contents in inputtable:opairs() do
 		table.insert(lines, ('\[%s\]'):format(section))
-		for key, value in pairs(contents) do
+		for key, value in contents:opairs() do
 			table.insert(lines, ('%s=%s'):format(key, value))
 		end
 		table.insert(lines, '')
 	end
 
-	file:write(table.concat(lines, '\r'))
+	file:write(table.concat(lines, '\n'))
 	file:close()
+end
+
+-- creates an interactable 'ordered table'
+function newT( t )
+   local mt = {}
+   -- set methods
+   mt.__index = {
+      -- set key order table inside __index for faster lookup
+      _korder = {},
+      -- traversal of hidden values
+      hidden = function() return pairs( mt.__index ) end,
+      -- traversal of table ordered: returning index, key
+      ipairs = function( self ) return ipairs( self._korder ) end,
+      -- traversal of table
+      pairs = function( self ) return pairs( self ) end,
+      -- traversal of table ordered: returning key,value
+      opairs = function( self )
+         local i = 0
+         local function iter( self )
+            i = i + 1
+            local k = self._korder[i]
+            if k then
+              return k,self[k]
+            end
+         end
+         return iter,self
+      end,
+      -- to be able to delete entries we must write a delete function
+      del = function( self,key )
+         if self[key] then
+            self[key] = nil
+            for i,k in ipairs( self._korder ) do
+               if k == key then
+                  table.remove( self._korder, i )
+                  return
+               end
+            end
+         end
+      end,
+   }
+   -- set new index handling
+   mt.__newindex = function( self,k,v )
+      if k ~= "del" and v then
+         rawset( self,k,v )
+         table.insert( self._korder, k )
+      end
+   end
+   return setmetatable( t or {},mt )
 end
 
 -- function to make logging messages less cluttered
@@ -158,180 +213,3 @@ function LogHelper(message, type)
 	end
 
 end
-
--- purely for debugging tables
-function print_r ( t )
-    local print_r_cache={}
-    local function sub_print_r(t,indent)
-        if (print_r_cache[tostring(t)]) then
-            LogHelper(indent.."*"..tostring(t), 'Debug')
-        else
-            print_r_cache[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        LogHelper(indent.."["..pos.."] => "..tostring(t).." {", 'Debug')
-                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        LogHelper(indent..string.rep(" ",string.len(pos)+6).."}", 'Debug')
-                    elseif (type(val)=="string") then
-                        LogHelper(indent.."["..pos..'] => "'..val..'"', 'Debug')
-                    else
-                        LogHelper(indent.."["..pos.."] => "..tostring(val), 'Debug')
-                    end
-                end
-            else
-                LogHelper(indent..tostring(t), 'Debug')
-            end
-        end
-    end
-    if (type(t)=="table") then
-        LogHelper(tostring(t).." {", 'Debug')
-        sub_print_r(t,"  ")
-        LogHelper("}", 'Debug')
-    else
-        sub_print_r(t,"  ")
-    end
-    print()
-end
-
--- ----------------------------------------------------------------------------------------------------------------
-
--- function SettingsProtocol()
---
---   LogHelper('Initiating Settings Protocol', 'Debug')
---
---   -- Create a LineNum/Key/Value matrix of the reference settings file
---   rTable = ReadIni(rFilePath)
---   if not rTable then
---     LogHelper('WTF? Reference settings file does not exist!', 'Error')
---     -- Set setup skin page and show
---     SKIN:Bang('!WriteKeyValue', 'Variables', 'page', 'refFileError')
---     SKIN:Bang('!Refresh')
---     SKIN:Bang('!ShowFade')
---   end
---
---   -- Create a LineNum/Key/Value matrix of the actual settings file
---   fTable = ReadIni(filePath)
---   if not fTable then -- if file is nonexistent
---     LogHelper('Settings file not found. Creating at ' .. filePath, 'Warning')
---     -- Create new file and add all values from reference file
---     local f = io.open(filePath,'w')
---     f:write('; MODERNGADGETS SETTINGS FILE\n\n', '[Variables]\n')
---     for sKey,a in ipairs(rTable) do f:write(a[1], '=', a[2], '\n') LogHelper('Added item \'' .. a[1] .. '\' with value \'' .. a[2] .. '\'', 'Debug') end
---     f:close()
---
---     -- Actually create the matrix
---     fTable = ReadIni(filePath)
---
---     LogHelper('Created settings file', 'Notice')
---
---     -- Set setup skin page and show
---     SKIN:Bang('!WriteKeyValue', 'Variables', 'page', 'welcome')
---     SKIN:Bang('!Refresh')
---     SKIN:Bang('!ShowFade')
---
---   end
---   -- Check if settings file is up-to-date
---   if (rTable[1][2] == fTable[1][2]) then LogHelper('Settings file is up-to-date', 'Notice')
---   else
---     LogHelper('Settings file is outdated! Initiating cross reference', 'Warning')
---     -- Create temporary file with current values
---     local tempF = io.open(tempFilePath,'w')
---     tempF:write('[Variables]\n')
---     for sKey,a in ipairs(fTable) do tempF:write(a[1], '=', a[2], '\n') LogHelper('Added item \'' .. a[1] .. '\' with value \'' .. a[2] .. '\' to \'' .. tempFilePath .. '\'', 'Debug') end
---     tempF:close()
---     -- Parse that file into a Key/Value table (NOT a matrix)
---     tempTable = ReadIniTable(tempFilePath)
---     if not tempTable then LogHelper('Something went wrong! Could not parse temporary file', 'Error') end
---     -- Delete settings file and create new one with updated settings
---     os.remove(filePath)
---     local setF = io.open(filePath,'w')
---     setF:write('; MODERNGADGETS SETTINGS FILE\n\n', '[Variables]\n', 'fileVersion=', rTable[1][2], '\n')
---     for sKey,a in ipairs(rTable) do
---       -- Setting needs to be added
---       if not tempTable[rTable[sKey][1]] then setF:write(rTable[sKey][1], '=', rTable[sKey][2], '\n')
---       -- Value has been changed
---       elseif not (rTable[sKey][2] == tempTable[rTable[sKey][1]] or sKey == 1) then
---         LogHelper('Conflict with value \'' .. rTable[sKey][1] .. '\' Current: \'' .. tempTable[rTable[sKey][1]] .. '\' Reference: \'' .. rTable[sKey][2] .. '\'', 'Debug')
---         setF:write(rTable[sKey][1], '=', tempTable[rTable[sKey][1]], '\n')
---       -- Not the first value (fileVersion)
---       elseif not (sKey == 1) then setF:write(rTable[sKey][1], '=', rTable[sKey][2], '\n') end
---     end
---     -- Close settings file and delete temporary file
---     LogHelper('Cross referencing complete. Cleaning up', 'Notice')
---     setF:close()
---     os.remove(tempFilePath)
---
---     -- Set setup skin page and show
---     SKIN:Bang('!WriteKeyValue', 'Variables', 'page', 'updated')
---     SKIN:Bang('!Refresh')
---     SKIN:Bang('!ShowFade')
---   end
---
--- end
---
--- -- Parses the given file into a LineNum/Key/Value matrix
--- function ReadIni(file)
---
---   LogHelper('ReadIni: Parsing file: ' .. file, 'Debug')
---
---   local f = io.open(file,'r')
---   if not f then return nil, LogHelper('Cannot open file: ' .. file, 'Error') end
---   local lineCounter = 0
---   local tablename = {}
---   local section
---   for fline in f:lines() do
---     lineCounter = lineCounter + 1
---     -- Ignore leading and trailing spaces
---     local line = fline:match("^%s*(.-)%s*$")
---     -- Ignore comments and section headers
---     if not line:match("^[%;#]") and not line:match("^%[(.*)%]$") and #line > 0 then
---       -- Parse Key=Value
---       local key, value = line:match("([^=]*)%=(.*)")
---       -- Remove white space from Key=Value
---       key = key:match("^%s*(%S*)%s*$")
---       value = value:match("^%s*(.-)%s*$")
---       -- Check for error
---       if not (key and value) then return nil, LogHelper('Bad key or value in file: ' .. file .. ': ' .. lineCounter .. '\n line: ' .. fline, 'Error') end
---       -- Set Key/Value in table
---       table.insert(tablename, {tostring(key), value})
---       -- LogHelper(key .. '=' .. value, 'Debug')
---     end
---   end
---   f:close()
---   return tablename
---
--- end
---
--- -- Parses the given file into a Key/Value table
--- function ReadIniTable(file)
---
---   LogHelper('ReadIniTable: Parsing file: ' .. file, 'Debug')
---
---   local f = io.open(file,'r')
---   if not f then return nil, LogHelper('Cannot open file: ' .. file, 'Error') end
---   local lineCounter = 0
---   local tablename = {}
---   local section
---   for fline in f:lines() do
---     lineCounter = lineCounter + 1
---     -- Ignore leading and trailing spaces
---     local line = fline:match("^%s*(.-)%s*$")
---     -- Ignore comments and section headers
---     if not line:match("^[%;#]") and not line:match("^%[(.*)%]$") and #line > 0 then
---       -- Parse Key=Value
---       local key, value = line:match("([^=]*)%=(.*)")
---       -- Remove white space from Key=Value
---       key = key:match("^%s*(%S*)%s*$")
---       value = value:match("^%s*(.-)%s*$")
---       -- Check for error
---       if not (key and value) then return nil, LogHelper('Bad key or value in file: ' .. file .. ': ' .. lineCounter .. '\n line: ' .. fline, 'Error') end
---       -- Set Key/Value in table
---       tablename[key] = value
---       -- LogHelper(key .. '=' .. value, 'Debug')
---     end
---   end
---   f:close()
---   return tablename
---
--- end
