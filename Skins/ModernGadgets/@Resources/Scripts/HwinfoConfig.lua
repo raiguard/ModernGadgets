cc64f2d4d20f7012fba6ab2fd13170b3af328bab
iamanai <iamanaiofficial@gmail.com>
2016-08-04 15:34:41 -0600

    Created HWiNFO config for All CPU Meter and GPU Meter, switched gadget references to external settings files

--- Skins/ModernGadgets/@Resources/Scripts/HwinfoConfig.lua
+++ Skins/ModernGadgets/@Resources/Scripts/HwinfoConfig.lua
@@ -5,3 +5,12 @@
 function Initialize() end
 
 function Update() end
+
+function FillIDs(prefix, suffix, base, hwinfoFilePath)
+
+  hexPrefix = base:match("(%w+)%w")
+  for i=1,7 do
+    print(prefix .. i .. suffix .. '=' .. hexPrefix .. i)
+    SKIN:Bang('!WriteKeyValue', 'Variables', prefix .. i .. suffix, hexPrefix .. i, hwinfoFilePath)
+  end
+end
