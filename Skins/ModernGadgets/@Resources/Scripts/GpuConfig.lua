60541719523d3a7625cd09eb2d159e39c2f544db
iamanai <iamanaiofficial@gmail.com>
2016-09-05 13:59:15 -0600

    Stuff

--- Skins/ModernGadgets/@Resources/Scripts/GpuConfig.lua
+++ Skins/ModernGadgets/@Resources/Scripts/GpuConfig.lua
@@ -8,6 +8,23 @@ function Update() end
 
 function ConfigGpuIcon(state)
 
-  if
+  if state == 'Intel' then
+    SKIN:Bang('!SetOption', 'CpuImage', 'ImageName', '#*imgPath*#cpu.png')
+    SKIN:Bang('!SetOption', 'CpuImage', 'ImageTint', '#*colorIntel*#')
+    SKIN:Bang('!SetOption', 'CpuImage', 'X', '(#*contentMargin*# + 1)')
+    SKIN:Bang('!SetOption', 'CpuImage', 'Y', '(#*contentMargin*# + 1)')
+    SKIN:Bang('!SetOption', 'CpuImage', 'W', '13')
+    SKIN:Bang('!SetOption', 'CpuImage', 'H', '13')
+    SKIN:Bang('!UpdateMeter', 'CpuImage')
+    SKIN:Bang('!Redraw')
+  elseif state ~= 'AMD' then
+    SKIN:Bang('!SetOption', 'CpuImage', 'ImageName', '#*imgPath*#cpu.png')
+    SKIN:Bang('!SetOption', 'CpuImage', 'X', '(#*contentMargin*# + 1)')
+    SKIN:Bang('!SetOption', 'CpuImage', 'Y', '(#*contentMargin*# + 1)')
+    SKIN:Bang('!SetOption', 'CpuImage', 'W', '13')
+    SKIN:Bang('!SetOption', 'CpuImage', 'H', '13')
+    SKIN:Bang('!UpdateMeter', 'CpuImage')
+    SKIN:Bang('!Redraw')
+  end
 
 end
