2b2cc24b756f4b8fd975192631e5474722dce8cd
iamanai <iamanaiofficial@gmail.com>
2016-11-06 14:56:52 -0700

    Finished new disks meter, separated out subpages

--- Skins/ModernGadgets/@Resources/Scripts/Config/DisksConfig.lua
+++ Skins/ModernGadgets/@Resources/Scripts/Config/DisksConfig.lua
@@ -1,97 +1,54 @@
--- MODERNGADGETS DISKS CONFIG SCRIPT
+-- MODERNGADGETS DISKS METER CONFIG SCRIPT
 -- Written by iamanai
 
 isDbg = true
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 function Initialize() end
 
 function Update() end
 
 -- configures the specified disk's meters and measures based on given input
-function ConfigureDisk(disk, diskType, baseColor, mode)
+function ConfigureDisk(disk, diskType, mode)
 
   histogramA = SKIN:GetVariable('histogramA')
   isHwinfoAvailable=tonumber(SKIN:GetVariable('isHwinfoAvailable'))
+  dynamicVarsPath = SKIN:GetVariable('dynamicVarsPath')
 
-  LogHelper('Configuring disk ' .. disk .. ' | diskType: ' .. diskType .. ' | baseColor: ' .. baseColor .. ' | mode: ' .. tostring(mode), 'Debug')
+  index = alphabet:find(disk)
+  prevDisk = alphabet:sub((index - 1), (index - 1))
+
+  LogHelper('Configuring disk ' .. disk .. ' | diskType: ' .. diskType .. ' | index: ' .. index .. ' | mode: ' .. tostring(mode), 'Debug')
 
   if mode then
     -- set disk hide variable
-    SKIN:Bang('!SetVariable', 'hideDisk' .. disk, '1')
-    SKIN:Bang('!WriteKeyValue', 'Variables', 'hideDisk' .. disk, '1')
-
-    -- disable all of the disk's measures that aren't related to sensing disk existence
-    SKIN:Bang('!DisableMeasureGroup', 'MgDisk' .. disk)
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Name', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Temp', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceUsedPercent', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'IdleTime', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Time', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Read', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Write', 'Disabled', '1')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Command', 'Disabled', '1')
-
-    -- set special Y values for proper positioning
-    SKIN:Bang('!SetOption', 'Disk' .. disk .. 'NameString', 'Y', '#*contentMargin*#')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Y', '#*contentMargin*#')
-    if disk < 10 then
-      SKIN:Bang('!SetOption', 'Disk' .. disk .. 'StorageBar', 'Y', '[Disk' .. (disk - 1) .. 'StorageBar:Y]')
-      SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Y', '[Disk' .. (disk - 1) .. 'StorageBar:Y]')
-    else
-      SKIN:Bang('!SetOption', 'Disk' .. disk .. 'StorageBar', 'Y', '([Disk' .. (disk - 1) .. 'StorageBar:Y] + 1)')
-      SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Y', '([Disk' .. (disk - 1) .. 'StorageBar:Y] + 1)')
-    end
-
-    -- hide all of the disk's meters
-    SKIN:Bang('!HideMeterGroup', 'MgDisk' .. disk)
-    SKIN:Bang('!HideMeter', 'Disk' .. disk .. 'TempString')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TimeString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'WriteImage', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'WriteString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'ReadImage', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'ReadString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageFractionString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StoragePercentString', 'Hidden', '1')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Hidden', '1')
-
-    -- set colors for the disk's histogram and line graph meters
-    SKIN:Bang('!SetOption', 'Disk' .. disk .. 'Histogram', 'PrimaryColor', '0,0,0,0')
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'Histogram', 'PrimaryColor', '0,0,0,0')
-
-    SKIN:Bang('!SetOption', 'GraphLines', 'LineColor' .. (10 - disk), '0,0,0,0')
-    SKIN:Bang('!WriteKeyValue', 'GraphLines', 'LineColor' .. (10 - disk), '0,0,0,0')
-
-    -- update all affected meters, redraw the skin
-    SKIN:Bang('!UpdateMeterGroup', 'MgDisk' .. disk)
-    SKIN:Bang('!UpdateMeterGroup', 'DiskStorageBars')
-    SKIN:Bang('!UpdateMeterGroup', 'Background')
-    SKIN:Bang('!Redraw')
-  else
-    -- set disk hide variable
     SKIN:Bang('!SetVariable', 'hideDisk' .. disk, '0')
-    SKIN:Bang('!WriteKeyValue', 'Variables', 'hideDisk' .. disk, '0')
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'hideDisk' .. disk, '0', dynamicVarsPath)
 
     -- enable all of the disk's measures that aren't related to sensing disk existence
-    SKIN:Bang('!EnableMeasureGroup', 'MgDisk' .. disk)
+    SKIN:Bang('!EnableMeasureGroup', 'Disk' .. disk)
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Name', 'Disabled', '0')
-    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Temp', 'Disabled', '0')
+    -- SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Temp', 'Disabled', '0')
+    -- SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceTotal', 'Disabled', '0')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceUsed', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceUsedPercent', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'IdleTime', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Time', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Read', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Write', 'Disabled', '0')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Activity', 'Disabled', '0')
     SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Command', 'Disabled', '0')
 
     -- set proper Y values
     SKIN:Bang('!SetOption', 'Disk' .. disk .. 'NameString', 'Y', '(#*rowSpacing*# + 4)r')
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Y', '(#*rowSpacing*# + 4)r')
+
     SKIN:Bang('!SetOption', 'Disk' .. disk .. 'StorageBar', 'Y', 'R')
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Y', 'R')
 
     -- show all of the disk's meters
-    SKIN:Bang('!ShowMeterGroup', 'MgDisk' .. disk)
+    SKIN:Bang('!ShowMeterGroup', 'Disk' .. disk)
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Hidden', '0')
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TimeString', 'Hidden', '0')
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'WriteImage', 'Hidden', '0')
@@ -103,70 +60,98 @@ function ConfigureDisk(disk, diskType, baseColor, mode)
     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Hidden', '0')
 
     -- show temperature meter if appropriate disk type
-    if diskType == 4 then
-      if isHwinfoAvailable == 1 then
-        SKIN:Bang('!ShowMeter', 'Disk' .. disk .. 'TempString')
-        SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '0')
-        SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', 'DiskTemps')
-        SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
-      else
-        SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', 'DiskTemps')
-        SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
-      end
+    -- if diskType == 4 then
+    --   if isHwinfoAvailable == 1 then
+    --     SKIN:Bang('!ShowMeter', 'Disk' .. disk .. 'TempString')
+    --     SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '0')
+    --     SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', 'DiskTemps')
+    --     SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
+    --   else
+    --     SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', 'DiskTemps')
+    --     SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
+    --   end
+    -- else
+    --   SKIN:Bang('!HideMeter', 'Disk' .. disk .. 'TempString')
+    --   SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '1')
+    --   SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', '')
+    --   SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
+    -- end
+  else
+    -- set disk hide variable
+    SKIN:Bang('!SetVariable', 'hideDisk' .. disk, '1')
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'hideDisk' .. disk, '1', dynamicVarsPath)
+
+    -- disable all of the disk's measures that aren't related to sensing disk existence
+    SKIN:Bang('!DisableMeasureGroup', 'Disk' .. disk)
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Name', 'Disabled', '1')
+    -- SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Temp', 'Disabled', '1')
+    -- SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceTotal', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceUsed', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'SpaceUsedPercent', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'IdleTime', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Time', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Read', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Write', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Activity', 'Disabled', '1')
+    SKIN:Bang('!WriteKeyValue', 'MeasureDisk' .. disk .. 'Command', 'Disabled', '1')
+
+    -- set special Y values for proper positioning
+    SKIN:Bang('!SetOption', 'Disk' .. disk .. 'NameString', 'Y', '#*contentMargin*#')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Y', '#*contentMargin*#')
+    if index > 1 then
+      SKIN:Bang('!SetOption', 'Disk' .. disk .. 'StorageBar', 'Y', '[Disk' .. prevDisk .. 'StorageBar:Y]')
+      SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Y', '[Disk' .. prevDisk .. 'StorageBar:Y]')
     else
-      SKIN:Bang('!HideMeter', 'Disk' .. disk .. 'TempString')
-      SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '1')
-      SKIN:Bang('!SetOption', 'Disk' .. disk .. 'TempString', 'Group', '')
-      SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'TempString')
+      SKIN:Bang('!SetOption', 'Disk' .. disk .. 'StorageBar', 'Y', '([DiskMetersMargin:Y])')
+      SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Y', '([DiskMetersMargin:Y])')
     end
 
-    -- set colors for the disk's histogram and line graph meters
-    SKIN:Bang('!SetOption', 'Disk' .. disk .. 'Histogram', 'PrimaryColor', GetStringRgb(baseColor) .. ',' .. histogramA)
-    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'Histogram', 'PrimaryColor', GetStringRgb(baseColor) .. ',' .. histogramA)
-
-    SKIN:Bang('!SetOption', 'GraphLines', 'LineColor' .. (10 - disk), baseColor)
-    SKIN:Bang('!WriteKeyValue', 'GraphLines', 'LineColor' .. (10 - disk), baseColor)
-
-    -- update all affected meters, redraw the skin
-    SKIN:Bang('!UpdateMeasureGroup', 'MgDisk' .. disk)
-    SKIN:Bang('!UpdateMeterGroup', 'MgDisk' .. disk)
-    SKIN:Bang('!UpdateMeterGroup', 'DiskTemps')
-    SKIN:Bang('!UpdateMeasure', 'MeasureHwinfoDetect')
-    SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'Histogram')
-    SKIN:Bang('!UpdateMeter', 'GraphLines')
-    SKIN:Bang('!UpdateMeterGroup', 'DiskStorageBars')
-    SKIN:Bang('!UpdateMeterGroup', 'Background')
-    SKIN:Bang('!Redraw')
+    -- hide all of the disk's meters
+    SKIN:Bang('!HideMeterGroup', 'Disk' .. disk)
+    SKIN:Bang('!HideMeter', 'Disk' .. disk .. 'TempString')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'NameString', 'Hidden', '1')
+    -- SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TempString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'TimeString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'WriteImage', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'WriteString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'ReadImage', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'ReadString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageFractionString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StoragePercentString', 'Hidden', '1')
+    SKIN:Bang('!WriteKeyValue', 'Disk' .. disk .. 'StorageBar', 'Hidden', '1')
   end
 
+  SetDiskColors()
+
+  -- update all affected meters, redraw the skin
+  SKIN:Bang('!UpdateMeasureGroup', 'Disk' .. disk)
+  SKIN:Bang('!UpdateMeterGroup', 'Disk' .. disk)
+  -- SKIN:Bang('!UpdateMeterGroup', 'DiskTemps')
+  -- SKIN:Bang('!UpdateMeasure', 'MeasureHwinfoDetect')
+  SKIN:Bang('!UpdateMeter', 'Disk' .. disk .. 'Histogram')
+  SKIN:Bang('!UpdateMeter', 'GraphLines')
+  SKIN:Bang('!UpdateMeterGroup', 'DiskStorageBars')
+  SKIN:Bang('!UpdateMeterGroup', 'Background')
+  SKIN:Bang('!Redraw')
+
 end
 
--- gets the RGB values from an RGBA string
-function GetStringRgb(source)
+function SetDiskColors()
 
-	local valueR = GetRgbaValue(source, 1)
-	local valueG = GetRgbaValue(source, 2)
-	local valueB = GetRgbaValue(source, 3)
+  local i = 0
 
-	return tostring(valueR .. ',' .. valueG .. ',' .. valueB)
-
-end
-
--- separates an RGBA value into the four values, and returns the value specified by 'key'
-function GetRgbaValue(source, key)
-
-	if key > 4 then
-		LogHelper('\'key\' cannot exceed 4!', 'Error')
-		return nil
-	else
-		rgbIt = string.gmatch(source,"%d+")
-		rgbTable = {}
-		for match in rgbIt do
-			table.insert(rgbTable, match)
-		end
-
-		return tostring(rgbTable[key])
-	end
+  alphabet:gsub(".", function(c)
+    local d = tonumber(SKIN:GetVariable('hideDisk' .. c))
+    if d == 0 then
+      i = i + 1
+      local color = SKIN:GetVariable('colorDisk' .. i)
+      SKIN:Bang('!SetVariable', 'colorDisk' .. c, color)
+      SKIN:Bang('!WriteKeyValue', 'Variables', 'colorDisk' .. c, color, dynamicVarsPath)
+      LogHelper('Set disk ' .. c .. ' color to: ' .. color .. ' | ' .. dynamicVarsPath, 'Debug')
+    else
+      SKIN:Bang('!SetVariable', 'colorDisk' .. c, '0,0,0,0')
+    end
+end)
 
 end
 
--- Skins/ModernGadgets/@Resources/Scripts/Settings/GadgetManager.lua
+++ Skins/ModernGadgets/@Resources/Scripts/Settings/GadgetManager.lua
@@ -6,7 +6,7 @@
 
 function Initialize()
 
-  welcomeSubpagePath=SKIN:GetVariable('welcomeSubpagePath')
+  gadgetManagerPath=SKIN:GetVariable('gadgetManagerPath')
   globalSettingsPath = SKIN:GetVariable('globalSettingsPath')
 
 end
@@ -19,10 +19,10 @@ function ToggleGadget(currentValue, gadgetPrefix)
 
   if currentValue == 0 then
     SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   SKIN:Bang('!ToggleConfig', 'ModernGadgets\\' .. gadgetPrefix, gadgetPrefix .. '.ini')
@@ -42,34 +42,34 @@ function UpdateToggleButtons()
 
   if cpuMeterState == 1 then
     SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   if networkMeterState == 1 then
     SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   if gpuMeterState == 1 then
     SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   if disksMeterState == 1 then
     SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   SKIN:Bang('!UpdateMeterGroup', 'GadgetButtons')
@@ -85,12 +85,12 @@ function ToggleNotifyUpdates(currentValue)
     SKIN:Bang('!SetVariable', 'notifyUpdates', '1')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'notifyUpdates', '1', globalSettingsPath)
     SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png', gadgetManagerPath)
   else
     SKIN:Bang('!SetVariable', 'notifyUpdates', '0')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'notifyUpdates', '0', globalSettingsPath)
     SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png', welcomeSubpagePath)
+    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png', gadgetManagerPath)
   end
 
   SKIN:Bang('!UpdateMeter', 'AutoNotifyUpdatesButton')
