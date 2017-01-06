54783f42443218754bab63a391954d2c1f12e500
iamanai <iamanaiofficial@gmail.com>
2017-01-05 22:08:35 -0700

    Fixed toggles in gadget manager

--- Skins/ModernGadgets/@Resources/Scripts/Settings/GadgetManager.lua
+++ Skins/ModernGadgets/@Resources/Scripts/Settings/GadgetManager.lua
@@ -18,11 +18,11 @@ function ToggleGadget(currentValue, gadgetPrefix)
   currentValue = tonumber(currentValue)
 
   if currentValue == 0 then
-    SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*toggleOnImg*#')
   else
-    SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', gadgetPrefix .. 'MeterButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', gadgetPrefix .. 'MeterButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   SKIN:Bang('!ToggleConfig', 'ModernGadgets\\' .. gadgetPrefix, gadgetPrefix .. '.ini')
@@ -41,35 +41,35 @@ function UpdateToggleButtons()
   disksMeterState=tonumber(SKIN:GetMeasure('MeasureDisksMeterState'):GetStringValue())
 
   if cpuMeterState == 1 then
-    SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*toggleOnImg*#')
   else
-    SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'CpuMeterButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'CpuMeterButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   if networkMeterState == 1 then
-    SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*toggleOnImg*#')
   else
-    SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'NetworkMeterButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'NetworkMeterButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   if gpuMeterState == 1 then
-    SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*toggleOnImg*#')
   else
-    SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'GpuMeterButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'GpuMeterButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   if disksMeterState == 1 then
-    SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*toggleOnImg*#')
   else
-    SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'DisksMeterButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'DisksMeterButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   SKIN:Bang('!UpdateMeterGroup', 'GadgetButtons')
@@ -84,13 +84,13 @@ function ToggleNotifyUpdates(currentValue)
   if currentValue == 0 then
     SKIN:Bang('!SetVariable', 'notifyUpdates', '1')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'notifyUpdates', '1', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*toggleOnImg*#')
   else
     SKIN:Bang('!SetVariable', 'notifyUpdates', '0')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'notifyUpdates', '0', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'AutoNotifyUpdatesButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'AutoNotifyUpdatesButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   SKIN:Bang('!UpdateMeter', 'AutoNotifyUpdatesButton')
@@ -106,13 +106,13 @@ function ToggleDevUpdates(currentValue)
   if currentValue == 0 then
     SKIN:Bang('!SetVariable', 'devUpdates', '1')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'devUpdates', '1', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'DevUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'DevUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'DevUpdatesButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'DevUpdatesButton', 'ImageName', '#*toggleOnImg*#')
   else
     SKIN:Bang('!SetVariable', 'devUpdates', '0')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'devUpdates', '0', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'DevUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'DevUpdatesButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'DevUpdatesButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'DevUpdatesButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   SKIN:Bang('!UpdateMeter', 'DevUpdatesButton')
@@ -128,13 +128,13 @@ function ToggleAutoBackups(currentValue)
   if currentValue == 0 then
     SKIN:Bang('!SetVariable', 'autoBackups', '1')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'autoBackups', '1', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'AutoBackupsButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoBackupsButton', 'ImageName', '#*imgPath*#Settings\\0a.png')
+    SKIN:Bang('!SetOption', 'AutoBackupsButton', 'ImageName', '#*toggleOnImg*#')
+    SKIN:Bang('!WriteKeyValue', 'AutoBackupsButton', 'ImageName', '#*toggleOnImg*#')
   else
     SKIN:Bang('!SetVariable', 'autoBackups', '0')
     SKIN:Bang('!WriteKeyValue', 'Variables', 'autoBackups', '0', globalSettingsPath)
-    SKIN:Bang('!SetOption', 'AutoBackupsButton', 'ImageName', '#*imgPath*#Settings\\0.png')
-    SKIN:Bang('!WriteKeyValue', 'AutoBackupsButton', 'ImageName', '#*imgPath*#Settings\\0.png')
+    SKIN:Bang('!SetOption', 'AutoBackupsButton', 'ImageName', '#*toggleOffImg*#')
+    SKIN:Bang('!WriteKeyValue', 'AutoBackupsButton', 'ImageName', '#*toggleOffImg*#')
   end
 
   SKIN:Bang('!UpdateMeter', 'AutoBackupsButton')
