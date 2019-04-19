ab39cc4005f89afd35754228eae32042bfabba65
raiguard <caleb.heuer@gmail.com>
2019-04-18 21:09:05 -0600

    Break the SunCalc script... I hate my life

--- Skins/ModernGadgets/@Resources/Scripts/SunCalc.lua
+++ Skins/ModernGadgets/@Resources/Scripts/SunCalc.lua
@@ -50,8 +50,8 @@ function GenerateData(timestamp, latitude, longitude, tzOffset)
 
     -- setup timestamps
     local timestamp, mDate, zDate, ysDate, tmDate = SetupTimestamps(timestamp, tzOffset)
-    RmLog('timestamp: ' .. timestamp)
-    RmLog('mDate: ' .. mDate .. ' zDate: ' .. zDate .. ' ysDate: ' .. ysDate .. ' tmDate: ' .. tmDate)
+    -- RmLog('timestamp: ' .. timestamp)
+    -- RmLog('mDate: ' .. mDate .. ' zDate: ' .. zDate .. ' ysDate: ' .. ysDate .. ' tmDate: ' .. tmDate)
 
     -- retrieve data tables from SunCalc script
     data.sunTimes = SunCalc.getTimes(mDate, latitude, longitude)
@@ -70,22 +70,18 @@ function GenerateData(timestamp, latitude, longitude, tzOffset)
     end
 
     -- convert timestamps back to FILETIME
-    data.sunTimes = UnixToFiletime(data.sunTimes, tzOffset)
-    data.moonTimes = UnixToFiletime(data.moonTimes, tzOffset)
+    data.sunTimes = ConvertTime(data.sunTimes, 'Windows', true, -6)
+    data.moonTimes = ConvertTime(data.moonTimes, 'Windows', true, tzOffset)
+    data.timestamps = ConvertTime({ mDate = mDate, zDate = zDate, ysDate = ysDate, tmDate = tmDate }, 'Windows', true)
+    data.timestamps.filetime = SKIN:GetMeasure('MeasureLocalTime'):GetValue()
 
     -- add moon phase name info
     data.moonIllumination.phaseName = GetMoonPhaseName(data.moonIllumination.phase)
 
-    -- add timestamp info for use in the skin
-    data.timestamps = UnixToFiletime({ timestamp = timestamp, mDate = mDate, zDate = zDate, ysDate = ysDate, tmDate = tmDate }, tzOffset)
-    data.julian_timestamps = {}
-
-    for k,v in pairs(data.timestamps) do
-        data.julian_timestamps[k] = toJulian(v)
-    end
-
     -- debug logging
+    RmLog('data = {')
     RmLog(data)
+    RmLog('}')
 
     SKIN:Bang('!EnableMeasureGroup', 'SunCalc')
     SKIN:Bang('!UpdateMeasureGroup', 'SunCalc')
@@ -112,11 +108,14 @@ function SetupTimestamps(timestamp, tzOffset)
 
     tzOffset = tzOffset or 0
     local localTz = (GetTimeOffset() / 3600)
-    -- RmLog('localTz: ' .. localTz .. ' | tzOffset: ' .. tzOffset)
+    -- convert Windows timestamp (0 = 1/1/1601) to Unix/Lua timestamp (0 = 1/1/1970)
+    RmLog(timestamp)
+    timestamp = ConvertTime(timestamp, 'Unix', false)
+    RmLog(timestamp)
     tDate = os.date("!*t", timestamp)
-    tDate.year = tDate.year - (1970 - 1601)
-    timestamp = os.time(tDate) - (os.date('*t')['isdst'] and 3600 or 0)
-    -- RmLog('timestamp: ' .. timestamp)
+    RmLog('tdate = {')
+    RmLog(tDate)
+    RmLog('}')
     mDate = tonumber(tostring(timestamp) .. '000')   
     zDate = tonumber(tostring(os.time{ year = tDate.year, month = tDate.month, day = tDate.day, hour = 0, min = 0, sec = 0 }) .. '000')
     ysDate = zDate - 86400000  
@@ -131,20 +130,23 @@ function SetupTimestamps(timestamp, tzOffset)
 
 end
 
--- converts unix epoch timestamps into Windows FILETIME timestamps
-function UnixToFiletime(table, tzOffset)
+function ConvertTime(n, to, mode, tzOffset)
 
-    for k,timestamp in pairs(table) do
+    if tzOffset == nil then tzOffset = 0 end
 
-        local tDate = os.date("*t", tostring(timestamp):sub(1,10))
-        tDate.year = tDate.year + (1970 - 1601)
-        tDate.hour = tDate.hour + tzOffset
-        timestamp = os.time(tDate)
-        table[k] = timestamp
+	local Formats = {
+		Unix    = -1,
+		Windows = 1
+    }
 
+    if type(n) == 'table' then
+        for k,t in pairs(n) do
+            n[k] = ConvertTime(t, to, mode, tzOffset)
+        end
+        return n
     end
-
-    return table
+    
+    return Formats[to] and (mode and tonumber(tostring(n):sub(1,10)) or n) + (11644473600 * Formats[to]) + (tzOffset * 3600) or nil
 
 end
 
@@ -177,14 +179,14 @@ function RmLog(message, category)
 
     if category == nil then category = 'Debug' end
     if category == 'Debug' and debug == false then return end
-    if printIndent == nil then printIndent = '' end
+    if printIndent == nil then printIndent = '    ' end
       
     if type(message) == 'table' then
         for k,v in pairs(message) do
             if type(v) == 'table' then
                 local pI = printIndent
                 RmLog(printIndent .. tostring(k) .. ' = {', category)
-                printIndent = printIndent .. '  '
+                printIndent = printIndent .. '    '
                 RmLog(v, category)
                 printIndent = pI
                 RmLog(printIndent .. '}', category)
@@ -521,8 +523,8 @@ SunCalc.getMoonTimes = function (date, lat, lng)
 
     result = {}
 
-    if rise then result.rise = hoursLater(t, rise) + (os.date('*t')['isdst'] and 3600000 or 0) end
-    if set then result.set = hoursLater(t, set) + (os.date('*t')['isdst'] and 3600000 or 0) end
+    if rise then result.rise = hoursLater(t, rise) end
+    if set then result.set = hoursLater(t, set) end
 
     if not rise and not set then result[ye > 0 and 'alwaysUp' or 'alwaysDown'] = true end
 
